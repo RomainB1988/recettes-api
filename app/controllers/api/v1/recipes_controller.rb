3d45c2854f96f0e685956376481570fa561fe393
Romain Buisine rom.buisine@gmail.com <rom.buisine@gmail.com>
2025-02-15 16:36:02 +0100

    Add recipes controller and routes

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,51 +1,48 @@
-class Api::V1::RecipesController < ApplicationController
-  module Api
-    module V1
-      class RecipesController < ApplicationController
-        before_action :set_recipe, only: [:show, :update, :destroy]
+module Api
+  module V1
+    class RecipesController < ApplicationController
+      before_action :set_recipe, only: [:show, :update, :destroy]
 
-        def index
-          @recipes = Recipe.all
-          render json: @recipes
+      def index
+        @recipes = Recipe.all
+        render json: @recipes
+      end
+
+      def show
+        render json: @recipe
+      end
+
+      def create
+        @recipe = Recipe.new(recipe_params)
+        if @recipe.save
+          render json: @recipe, status: :created
+        else
+          render json: @recipe.errors, status: :unprocessable_entity
         end
+      end
 
-        def show
+      def update
+        if @recipe.update(recipe_params)
           render json: @recipe
+        else
+          render json: @recipe.errors, status: :unprocessable_entity
         end
+      end
 
-        def create
-          @recipe = Recipe.new(recipe_params)
-          if @recipe.save
-            render json: @recipe, status: :created
-          else
-            render json: @recipe.errors, status: :unprocessable_entity
-          end
-        end
+      def destroy
+        @recipe.destroy
+        head :no_content
+      end
 
-        def update
-          if @recipe.update(recipe_params)
-            render json: @recipe
-          else
-            render json: @recipe.errors, status: :unprocessable_entity
-          end
-        end
+      private
 
-        def destroy
-          @recipe.destroy
-          head :no_content
-        end
+      def set_recipe
+        @recipe = Recipe.find(params[:id])
+      end
 
-        private
-
-        def set_recipe
-          @recipe = Recipe.find(params[:id])
-        end
-
-        def recipe_params
-          params.require(:recipe).permit(:title, :description, :ingredients, :steps, :user_id)
-        end
+      def recipe_params
+        params.require(:recipe).permit(:title, :description, :ingredients, :steps, :user_id)
       end
     end
   end
-
 end
