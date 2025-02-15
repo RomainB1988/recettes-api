e0f1448b6175cc586726da3eecbe9771e7f34494
Romain Buisine rom.buisine@gmail.com <rom.buisine@gmail.com>
2025-02-15 16:36:27 +0100

    Add CommentsController with index, create, and destroy actions

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,46 @@
-class Api::V1::CommentsController < ApplicationController
+module Api
+  module V1
+    class CommentsController < ApplicationController
+      before_action :set_recipe, only: [:index, :create]
+      before_action :set_comment, only: [:destroy]
+
+      def index
+        @comments = @recipe.comments
+        render json: @comments
+      end
+
+      def create
+        @comment = @recipe.comments.build(comment_params)
+        @comment.user = current_user
+
+        if @comment.save
+          render json: @comment, status: :created
+        else
+          render json: @comment.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        if @comment.destroy
+          head :no_content
+        else
+          render json: @comment.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def set_recipe
+        @recipe = Recipe.find(params[:recipe_id])
+      end
+
+      def set_comment
+        @comment = Comment.find(params[:id])
+      end
+
+      def comment_params
+        params.require(:comment).permit(:content)
+      end
+    end
+  end
 end
